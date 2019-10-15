4807f819eea70ac371fa8e9eac319736dfb46d9d
willnx <willnx84@gmail.com>
2019-10-15 10:29:25 -0700

    fixed line continuation indention

--- container_shell/lib/dockage.py
+++ container_shell/lib/dockage.py
@@ -144,9 +144,9 @@ def container_command(username, user_uid, user_gid, create_user, command, runuse
 
         fix_pty_ownership = 'chown {0}:{0} /dev/pts/0 2>/dev/null'.format(username)
         everything = "/bin/bash -c '{0} && {1} && {2} ; {3}'".format(make_group,
-                                                                      make_user,
-                                                                      fix_pty_ownership,
-                                                                      run_user)
+                                                                     make_user,
+                                                                     fix_pty_ownership,
+                                                                     run_user)
     elif command:
         everything = command
     else:
